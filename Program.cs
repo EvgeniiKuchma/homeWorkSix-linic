d00cb88dfaeed1a7667bdb2e5dbc94a0989855b5
Евгений Кучма <desferto87@gmail.com>
2024-06-16 22:21:05 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkSixСlinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int patients;
+            Console.Write($"Введите кол-во пациентов: ");
+            patients = Convert.ToInt32( Console.ReadLine() );
+            int appointmenTime = 10;
+            int minutesInHours = 60;
+            int queueTimeHours = patients * appointmenTime / minutesInHours;
+            int queueTimeMinutes = patients * appointmenTime % minutesInHours;
+            Console.WriteLine($"Вы должны отстоять в очереди {queueTimeHours} " +
+                $"часов и {queueTimeMinutes} минут.");
+        }
+    }
+}
